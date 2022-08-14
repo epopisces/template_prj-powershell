b2fb534571117322eb73494cfb0848f1f0604f73
epopisces <epopisces@gmail.com>
2022-08-14 14:11:20 -0500

    template v1

--- Verb-Noun.ps1
+++ Verb-Noun.ps1
@@ -29,7 +29,20 @@ Repeat the .LINK keyword for each related topic.
 ################################################################################
 #region #*      User-definable Variables
 ################################################################################
-$MenuItem = "Run", "Edit", "View", "Delete" # example menu item list
+#? The following variables can be modified by the user.
+$ConfigFilePath = "config.conf" # default config file path
+$LogFolderPath = "logs"
+#endregion
+
+
+################################################################################
+#region #*      Global Variables
+################################################################################
+
+$Menu = [ordered]@{
+  "Header1" = "Run", "Edit", "View", "Delete"; # example menu item list
+  "Header2" = "Create", "Read", "Update", "Delete"; # example menu item list
+}
 
 #endregion
 
@@ -38,13 +51,14 @@ $MenuItem = "Run", "Edit", "View", "Delete" # example menu item list
 #region #*      Logging Initialization
 ################################################################################
 
-$LogFolder = "logs"
-if ( -not ( Test-Path $LogFolder ) ) {
-    New-Item $FolderName -ItemType Directory
+
+if ( -not ( Test-Path $LogFolderPath ) ) {
+    New-Item $LogFolderPath -ItemType Directory
     Write-Host "Created logging folder in local directory"
 }
 
-Start-Transcript -Append "$LogFolder\$(Get-Date -f yyyy-MM-dd)_$$Verb-$$Noun.txt"
+#! Modify to match module name
+Start-Transcript -Append "$LogFolderPath\$(Get-Date -f yyyy-MM-dd)_Verb-Noun.txt"
 
 #endregion
 
@@ -53,10 +67,11 @@ Start-Transcript -Append "$LogFolder\$(Get-Date -f yyyy-MM-dd)_$$Verb-$$Noun.txt
 #region #*      Imports & Dependency Handling
 ################################################################################
 
-If ( -not ( Get-Module -ListAvailable -Name ModuleName ) ) {
-  Install-Module ModuleName
-  Import-Module ModuleName
-}
+#! Modify to include any dependencies
+# If ( -not ( Get-Module -ListAvailable -Name ModuleName ) ) {
+#   Install-Module ModuleName
+#   Import-Module ModuleName
+# }
 #endregion
 
 
@@ -68,36 +83,15 @@ function Get-Config {
   param (
     [string]$ConfigFilePath
   )
-  Get-Content $ConfigFilePath | ForEach-Object -Begin { $Config=@{} } -process { 
-    $Key = [regex]::split($_,'='); If( ( $Key[0].CompareTo("") -ne 0 ) -and ( $Key[0].StartsWith("[") -ne $True ) ) { 
-      $Config.Add($Key[0], $Key[1])
-    }
-  }
-}
+  # Get-Content $ConfigFilePath | ForEach-Object -Begin { $Config=@{} } -process { 
+  #   $Key = [regex]::split($_,'='); If( ( $Key[0].CompareTo("") -ne 0 ) -and ( $Key[0].StartsWith("[") -ne $True ) ) { 
+  #     $Config.Add($Key[0], $Key[1])
+  #   }
+  # }
 
-#endregion
+  $Config = Get-Content -Path $ConfigFilePath | ConvertFrom-Json
 
-
-################################################################################
-#region #*      User Menu (if needed)
-################################################################################
-function Show-Menu {
-  param (
-    [string]$Title = 'My Menu',
-    [string[]]$MenuItems
-  )
-
-  Clear-Host
-  Write-Host "============================================================"
-  Write-Host $MenuTitle
-  Write-Host "============================================================"
-  
-  ForEach ( $MenuItem in $MenuItems ) {
-    Write-Host $array.IndexOf($item) + ": " + $MenuItem
-  }
-  Write-Host "Q. Quit"
-  
-  Write-Host "============================================================"
+  return $Config
 }
 
 #endregion
@@ -106,6 +100,47 @@ function Show-Menu {
 ################################################################################
 #region #*      Supporting Functions
 ################################################################################
+function Verb-Noun {
+  [CmdletBinding()]
+  param (
+    
+  )
+  
+}
+#endregion
+
+
+################################################################################
+#region #*      User Menu (if needed)
+################################################################################
+function Show-Menu {
+  param (
+    [string]$MenuTitle = 'My Menu',
+    $Menu
+  )
+
+  $CenteredTitle = ("| $(" " * ( ( 55 - $MenuTitle.Length ) / 2 ) ) $MenuTitle $( " " * ( ( 55 - $MenuTitle.Length ) / 2 ) )").substring(0, 59)+"|"
+
+  Clear-Host
+  Write-Host "|============================================================|"
+  Write-Host $CenteredTitle
+  Write-Host "|============================================================|"
+  
+  $MenuIndex = 0
+  ForEach ( $MenuSection in $Menu.Keys ) {
+    Write-Host ("|$(" " * ( ( 56 - $MenuSection.Length ) / 2)) $MenuSection $(" " * ( ( 56 - $MenuSection.Length ) / 2))").substring(0, 58)"|"
+    
+    ForEach ( $MenuItem in $Menu[$MenuSection] ) {
+      $MenuIndex++
+      Write-Host ("| $MenuIndex. $MenuItem").PadRight(58," ")"|"
+    }
+    
+    Write-Host "|                                                           |"
+  }
+
+  
+  Write-Host "============================================================"
+}
 
 #endregion
 
@@ -114,35 +149,29 @@ function Show-Menu {
 #region #*      Main Program Loop
 ################################################################################
 
-function Verb-Noun {
-  [CmdletBinding()]
-  param (
-    
-  )
-  
-  # Load Config File
-  Get-Config -ConfigFilePath "config.conf"
+$Config = Get-Config -ConfigFilePath $ConfigFilePath
 
-  do {
-    Show-Menu -MenuItems $MenuItems
-    $selection = (Read-Host "Select an option: ").toLower()
-    switch ($selection)
-    {
-      '1' {
-        # do thing here (eg assign var, call function, etc)
-        'You chose option #1'
-      } '2' {
-        # do thing here (eg assign var, call function, etc)
-        'You chose option #2'
-      } '3' {
-        # do thing here (eg assign var, call function, etc)
-        'You chose option #3'
-      }
+
+do {
+  Show-Menu -MenuItems $MenuItems
+  $selection = (Read-Host "Select an option: ").toLower()
+  switch ($selection)
+  {
+    '1' {
+      # do thing here (eg assign var, call function, etc)
+      'You chose option #1'
+    } '2' {
+      # do thing here (eg assign var, call function, etc)
+      'You chose option #2'
+    } '3' {
+      # do thing here (eg assign var, call function, etc)
+      'You chose option #3'
     }
-    pause
   }
-  until ($selection -eq 'q')
+  pause
 }
+until ($selection -eq 'q')
+
 
 #endregion
 
